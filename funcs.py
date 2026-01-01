e6eb77e5e7c67a0df0b4a08a58ee115ded773a78
grae ceney <gceney7@gmail.com>
2026-01-01 18:52:04 +0000

    Initialize empty training, data class, and functions scripts

---
+++ funcs.py
@@ -0,0 +1,10 @@
+
+#imports
+
+
+
+#training script
+    #this is a somewhat copy of the original training script from the 'price-prediction' repo i made but refactored for images
+def train_model():
+    pass
+
